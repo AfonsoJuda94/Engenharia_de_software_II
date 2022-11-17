c2b3dea80b7f57f551c06440a0950bc4de7aac7d
antonioFilho-global <antonio.afce@gmail.com>
2022-11-17 20:26:57 -0300

    adição de verificação de token

--- app.js
+++ app.js
@@ -18,6 +18,46 @@ app.get('/', (req, res) =>{
     res.status(200).json({ msg: 'Bem vindo a nossa API!'})
 })
 
+//Private Route
+app.get('/users/:id', checkToken, async (req, res) =>{
+    
+    const id = req.params.id
+
+    //Check if user exist || Checar se o usuário existe
+    //Pegando as informações por id, menos o password por questão de segurança
+    const user = await User.findById(id, '-password')
+
+    if(!user) {
+        return res.status(404).json({ msg: 'Usuário não encontrado' })
+    }
+
+    /* Retornando os dados do usuário, menos o password por segurança */
+    res.status(200).json({ user })
+})
+
+//Continuar daqui 
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    /* Pegando o token do Header */
+    const token = authHeader && authHeader.split(' ')[1]
+
+    /* Caso o usuário não tenha token */
+    if(!token) {
+        return res.status(401).json({ msg: 'Acesso negado!'})
+    }
+
+    /* Validando se o token é válido */
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        /* Caso a verificação do token passar é liberado o acesso a rota */ 
+        next()
+
+    } catch(error) {
+        res.status(400).json({msg: "Token inválido!"})
+    }
+}
+
 //Register User - Registro de usuário
 app.post('/auth/register', async(req, res) => {
 
@@ -109,8 +149,28 @@ app.post("/auth/login", async (req, res) => {
 
     if(!checkPassword) {
         return res.status(422).json({ msg: 'Senha inválida' })
-     }
+    }
+    
+    try {
+        
+        const secret = process.env.SECRET
+        
+        const token = jwt.sign({
+                id: user._id,
+            },
+            secret,
+        )
 
+        res.status(200).json({msg: "Autenticação realizada com sucesso", token })
+
+    } catch(error) {
+        console.log(error)
+        res
+            .status(500)
+            .json({
+                msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!'
+            })
+    }
 })
 
 //Credencials de conexão ao banco 
