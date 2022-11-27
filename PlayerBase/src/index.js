250b88aa803f9afd850dce73500492a49ad75285
AfonsoJuda94 <97704547+AfonsoJuda94@users.noreply.github.com>
2022-11-27 01:43:36 -0300

    Add files via upload

---
+++ PlayerBase/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import BarraPrincipal from './Components/BarraPrincipal';
+import Principal from './Components/Principal';
+import {RouterProvider,createBrowserRouter,BrowserRouter,Route,Routes, Link} from 'react-router-dom';
+import Login from './Components/Login';
+import Registro from './Components/Registro';
+//import routes from './Components/routes';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router =createBrowserRouter([{
+    path:'/',
+    element: <Principal/>
+},
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/Registro',
+    element: <Registro/>
+  }
+])
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+
+    
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
